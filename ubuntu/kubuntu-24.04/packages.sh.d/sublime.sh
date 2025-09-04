911168986db6a98c6664e1d7af64a41b7801f7f1
f1rex <powerbq@gmail.com>
2025-09-04 19:24:25 +0300

    ...

--- ubuntu/kubuntu-24.04/packages.sh.d/sublime.sh
+++ ubuntu/kubuntu-24.04/packages.sh.d/sublime.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exit 0
+
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list > /dev/null
 
